aad078ebda75f040c2ea12bf25c182c5f071580e
Derek Maciel <derekamaciel@gmail.com>
2016-08-19 00:27:52 -0400

    Some youtube-dl testing

--- plugins/audio.js
+++ plugins/audio.js
@@ -1,26 +1,41 @@
 "use strict";
 
 const client = require('../client');
+const fs = require('fs');
+const youtubedl = require('youtube-dl');
+const logger = require('../logger');
 
 exports.COMMANDS = {
-    "ping": {
-        help: "Respond with 'pong'",
-        callback: ping
-    },
-    "test": {
-        help: "Test play a song",
-        callback: test
+    "playy": {
+        help: "Play a youtube video",
+        callback: play
     }
 };
 
-function ping(message, args) {
-    client.reply(message, "pong! :)");
-}
+function play(message, args) {
+    if (args['_'].length == 0) {
+        client.reply(message, "Play what? :confused:");
+        return;
+    }
 
-function test(message, args) {
-    client.joinVoiceChannel("166094007712088064").then(function(value) {
-            client.voiceConnection.playFile('./Mad World - Gary Jules-4N3N1MlvVc4.webm');
-        }, function (reason) {
-            logger.info(`Could not join voice channel: ${reason}`);
+    var video = youtubedl(args['_'][0], [
+        '--extract-audio', 
+        '--audio-format', 'mp3'
+    ]);
+
+    video.on('info', function(info) {
+        console.log('Download started');
+        console.log('filename: ' + info.filename);
+        console.log('size: ' + info.size)
+    });
+
+    video.pipe(fs.createWriteStream('../audio-cache/testing.mp3'));
+
+    client.joinVoiceChannel("166094007712088064", function(error, connection) {
+        if (error) {
+            logger.info(`Could not join voice channel: ${error}`);
+        } else {
+            client.voiceConnection.playFile('../audio-cache/testing.mp3');
+        }
     });
 }
