ac83735d16a825c32464c3f09ab55bb1b830edff
Derek Maciel <derekamaciel@gmail.com>
2016-08-20 00:24:28 -0400

    Reuse cached video files

--- plugins/audio.js
+++ plugins/audio.js
@@ -24,33 +24,46 @@ function play(message, args) {
     var path;
 
     youtubedl.getInfo(url, [], function(error, info) {
-        if (error) {
-            console.warn(`Failed to get info for url ${url}: ${error}`);
-            return;
-        }
+        if (error) return console.warn(`Failed to get info for url ${url}: ${error}`);
 
         var megs = (info.size / Math.pow(1024, 2)).toFixed(2);
 
         hash.update(info._filename);
         var filename = hash.digest('hex');
         path = `audio-cache/${filename}.mp3`;
-        
-        logger.info(`Downloading ${info._filename} (${megs}mb) to ${path}`);
-    });
 
+        fs.access(path, fs.constants.F_OK, function (error) {
+            if (error) {
+                logger.info(`Downloading video (${megs}mb) to ${path}`);
+                download_file_then_play(url, path);
+            } else {
+                logger.debug(`Video already downloaded (${path}), using cached file`);
+                play_file(path);
+            }
+        });
+    });
+}
+
+function download_file_then_play(url, path) {
     var video = youtubedl(url, ['--extract-audio', '--audio-format', 'mp3']);
 
     var stream = fs.createWriteStream(path);
-    video.pipe(stream);    
+    video.pipe(stream);
 
     video.on('end', function() {
         logger.info(`Download complete.`);
-        client.joinVoiceChannel("166094007712088064", function(error, connection) {
-            var p = client.voiceConnection.playFile(path, {volume: 0.25});
-        });
+        play_file(path);
     });
 
     video.on('error', function(error) {
         logger.info(`Could not download video: ${error}`);
+    });
+}
+
+function play_file(path) {
+    client.joinVoiceChannel("166094007712088064", function(error, connection) {
+        if (error) return logger.warn(`Could not join voice channel: ${error}`);
+
+        connection.playFile(path, {volume: 0.25});
     });
 }
